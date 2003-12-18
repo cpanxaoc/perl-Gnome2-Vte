bec4615a62687b348ea778557d605eeb6b5fdbc9
Torsten Schönfeld <tsch@src.gnome.org>
2003-12-18 21:19:46 +0000

    Mention Gnome2::Vte::index.
    	* Vte.pm: Mention Gnome2::Vte::index.
    
    svn path=/trunk/; revision=11

--- Vte.pm
+++ Vte.pm
@@ -44,7 +44,8 @@ library (libvte for short).
 
 =head1 SEE ALSO
 
-L<Gtk2>, L<Gtk2::api> and L<http://developer.gnome.org/doc/API/2.0/vte/>.
+L<Gnome2::Vte::index>(3pm), L<Gtk2>(3pm), L<Gtk2::api>(3pm) and
+L<http://developer.gnome.org/doc/API/2.0/vte/>.
 
 =head1 AUTHOR
 
